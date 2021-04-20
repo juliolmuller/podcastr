8ae2fde1069e6bb2afe54c2f6f11ac622547d3d9
Julio Muller <juliodolago@gmail.com>
2021-04-20 00:41:14 -0300

    Configured TypeScript and ESlint

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    '@lacussoft/typescript',
+  ],
+  ignorePatterns: [],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: [
+    'react',
+    'react-hooks',
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debug': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'react/jsx-closing-bracket-location': 'error',
+    'react/jsx-indent': ['error', 2, {
+      checkAttributes: true,
+      indentLogicalExpressions: true,
+    }],
+    'react/jsx-indent-props': ['error', 2],
+    'react/prop-types': 'off',
+  },
+}
