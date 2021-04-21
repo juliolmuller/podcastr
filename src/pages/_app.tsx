8a7541bb4ec5cafd820f2f57baaf0ab0cb8c1284
Julio Muller <juliodolago@gmail.com>
2021-04-20 23:19:14 -0300

    Created global styles file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
+import '../assets/styles.scss'
 
-function MyApp({ Component, pageProps }) {
+function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default App
