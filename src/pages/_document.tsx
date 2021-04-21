46d8fb3af113f8960fd0132010337f58f8a432ca
Julio Muller <juliodolago@gmail.com>
2021-04-20 23:18:29 -0300

    Added custom fonts

---
+++ src/pages/_document.tsx
@@ -0,0 +1,22 @@
+import BaseDocument, { Html, Head, Main, NextScript } from 'next/document'
+
+/* eslint-disable class-methods-use-this */
+class Document extends BaseDocument {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="shortcut icon" href="favicon.png" type="image/png"/>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default Document
