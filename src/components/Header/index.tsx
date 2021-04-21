c44da3d7ab694e487a8964c9bf09c21a04e22c1c
Julio Muller <juliodolago@gmail.com>
2021-04-20 23:50:30 -0300

    Configured component to display current date dynamically

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,17 @@
+import { format } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
 import styles from './styles.module.scss'
 
 function Header() {
+  const currentDate = format(new Date(), 'EEEEEE, d MMM', { locale: ptBR })
+
   return (
     <header className={styles.container}>
       <img src="/img/logo.svg" alt="logo PodShare" />
 
       <p>O melhor para vc ouvir, sempre!</p>
 
-      <span>Qui, 8 de abril</span>
+      <span>{currentDate}</span>
     </header>
   )
 }
