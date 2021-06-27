95d3330bad3032fc6a96858c205700554569209a
Julio Muller <juliodolago@gmail.com>
2021-06-27 01:15:58 -0300

    Refactor "_app" component

---
+++ src/pages/_app/index.tsx
@@ -0,0 +1,28 @@
+import NextApp from 'next/app'
+import ContextProvider from '~/contexts'
+import Header from '~/components/Header'
+import Player from '~/components/Player'
+
+import '~/global-styles.scss'
+import styles from './styles.module.scss'
+
+class App extends NextApp {
+  render() {
+    const { Component, pageProps } = this.props
+
+    return (
+      <div className={styles.wrapper}>
+        <ContextProvider>
+          <main>
+            <Header />
+            <Component {...pageProps} />
+          </main>
+
+          <Player />
+        </ContextProvider>
+      </div>
+    )
+  }
+}
+
+export default App
