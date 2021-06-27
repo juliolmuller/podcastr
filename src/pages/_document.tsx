3c4cce5052cb07eaa9f23a31d18ebd0bcbf2e6af
Julio Muller <juliodolago@gmail.com>
2021-06-27 01:16:10 -0300

    Refactor "_document" component

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,12 @@
+/* eslint-disable @next/next/no-page-custom-font */
 import BaseDocument, { Html, Head, Main, NextScript } from 'next/document'
 
-/* eslint-disable class-methods-use-this */
 class Document extends BaseDocument {
-  render() {
+  render() { // eslint-disable-line class-methods-use-this
     return (
-      <Html>
+      <Html lang="pt-BR">
         <Head>
+          <meta name="description" content="Aplicação para compartilhamento de podcasts" />
           <link rel="shortcut icon" href="/favicon.png" type="image/png"/>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap" />
